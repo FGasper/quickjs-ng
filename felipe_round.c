82c908a4b516256ad347e195506522fba6b34752
Felipe Gasper <felipe@felipegasper.com>
2023-12-06 22:43:27 -0500

    round check

--- felipe_round.c
+++ felipe_round.c
@@ -4,11 +4,25 @@
 
 int main()
 {
-    double d = 25;
+    double d[] = {26, 25, 24};
 
     fesetround(FE_UPWARD);
 
-    printf("round %f up w printf: %+.*e\n", d, 0, d);
+    for (int i=0; i<sizeof(d) / sizeof(double); i++) {
+            printf("round %f up w printf: %+.*e\n", d[i], 0, d[i]);
+    }
+
+    fesetround(FE_DOWNWARD);
+
+    for (int i=0; i<sizeof(d) / sizeof(double); i++) {
+            printf("round %f down w printf: %+.*e\n", d[i], 0, d[i]);
+    }
+
+    fesetround(FE_TONEAREST);
+
+    for (int i=0; i<sizeof(d) / sizeof(double); i++) {
+            printf("round %f to nearest w printf: %+.*e\n", d[i], 0, d[i]);
+    }
 
     return 0;
 }
